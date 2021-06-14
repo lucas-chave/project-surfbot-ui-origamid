795f25085c28042c452efdffe76c1ed74fdb5072
lucas-chaves <lcchavesr150@gmail.com>
2021-06-14 11:17:33 -0300

    add initial description component

---
+++ surfbot-origamid/src/components/Description.jsx
@@ -0,0 +1,11 @@
+import '../css/description.css'
+
+function Description() {
+  return (
+    <div className="container-description-initial">
+      <p>Aula na Praia Mansa de Sexta a Domingo dàs 06:00 às 14:00</p> 
+    </div>
+  );
+}
+
+export default Description;
