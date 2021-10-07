f21e14918d2c66f79cf7ec1f10ee7b7de200bf01
lucas-chave <lcchavesr150@gmail.com>
2021-10-07 11:10:06 -0300

    change in font size

--- surfbot-origamid/src/components/SectionInfor.jsx
+++ surfbot-origamid/src/components/SectionInfor.jsx
@@ -1,30 +1,31 @@
 import ImageS2 from './ImageS2'
 import image from '../images/Rectangle.png'
 import map from '../images/Group.png'
-import '../css/imageInfor.css'
 import Heading from './Heading'
 import CardList from './CardList'
 import Paragraph from './Paragraph'
 import TextInfor from './TextInfor'
+import '../css/imageInfor.css'
 
 function SectionInfor() {
-  const text = (`Lorem Ipsum is simply dummy text of the printing and 
-                typesetting industry. Lorem Ipsum has been the industry's 
-                standard dummy text ever since the 1500s, when an
-                unknown printer took a galley of type and scrambled
-                it to make a type specimen book.
-                Lorem Ipsum is simply dummy text of the printing and 
-                typesetting industry. Lorem Ipsum has been the industry's 
-                standard dummy text ever since the 1500s, when an
-                unknown printer took a galley of type and scrambled
-                it to make a type specimen book.
-                it to make a type specimen book.
-                Lorem Ipsum is simply dummy text of the printing and 
-                typesetting industry. Lorem Ipsum has been the industry's 
-                standard dummy text ever since the 1500s, when an
-                unknown printer took a galley of type and scrambled
-                it to make a type specimen book.
-                `)
+  const text = (
+    `Lorem Ipsum is simply dummy text of the printing and 
+    typesetting industry. Lorem Ipsum has been the industry's 
+    standard dummy text ever since the 1500s, when an
+    unknown printer took a galley of type and scrambled
+    it to make a type specimen book.
+    Lorem Ipsum is simply dummy text of the printing and 
+    typesetting industry. Lorem Ipsum has been the industry's 
+    standard dummy text ever since the 1500s, when an
+    unknown printer took a galley of type and scrambled
+    it to make a type specimen book.
+    it to make a type specimen book.
+    Lorem Ipsum is simply dummy text of the printing and 
+    typesetting industry. Lorem Ipsum has been the industry's 
+    standard dummy text ever since the 1500s, when an
+    unknown printer took a galley of type and scrambled
+    it to make a type specimen book.
+`)
   return(
     <div className="container-section">
       <div className="container-left">
