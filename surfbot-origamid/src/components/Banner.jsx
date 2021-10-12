56f6244c218fafe5bbe91d0c3132c58e5a41340b
lucas-chave <lcchavesr150@gmail.com>
2021-10-12 11:18:03 -0300

    addig styles in banner

--- surfbot-origamid/src/components/Banner.jsx
+++ surfbot-origamid/src/components/Banner.jsx
@@ -1,22 +1,26 @@
 import '../css/banner.css';
+import imag from '../fotos/foto-srf.svg'
 
 function Banner() {
   return (
     <div className="container-banner">
-      <h1>Escola de Surf</h1>
-      <form className="form-email">
-        <label htmlFor="email">
-        <input
-          className="input-email"
-          placeholder="Email" 
-          type="email"
-          name="email"
-          id="email"
-        />
-        </label>
-        <button className="button-register">MATRICULE-SE</button>
-      </form>
-    </div>
+        <img src={ imag } alt="foto do mar com dois rapazes em cima de pranchas" className="foto-banner"/>
+        <div className="container">
+        <h1>Escola de Surf</h1>
+        <form className="form-email">
+          <label htmlFor="email">
+          <input
+            className="input-email"
+            placeholder="Email" 
+            type="email"
+            name="email"
+            id="email"
+            />
+          </label>
+          <button className="button-register">MATRICULE-SE</button>
+        </form>
+        </div> 
+      </div>
   );
 }
 
