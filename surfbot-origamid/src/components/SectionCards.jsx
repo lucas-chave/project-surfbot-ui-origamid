0b7aa1c86a09d58314ce7604c00e6f0c80359189
lucas-chave <lcchavesr150@gmail.com>
2021-10-13 23:27:29 -0300

    adding cards with lesson information

--- surfbot-origamid/src/components/SectionCards.jsx
+++ surfbot-origamid/src/components/SectionCards.jsx
@@ -1,9 +1,69 @@
 import React from 'react';
+import waves from '../images/ondas.svg';
+import pran1 from '../images/surfbot-icon.svg';
+import pran2 from '../images/Group1.svg';
+import pran3 from '../images/Group2.svg';
+import setButton from '../images/seta.svg';
+import '../css/sectionCards.css'
 
 function SectionCads() {
+  const cardsData = [
+    {
+      image: pran1,
+      title: 'INFANTIL',
+      tags: [
+        'Público entre 5 e 15 anos',
+        'equipamentos fornecidos',
+        'Horários de sexta a sábado',
+        '2 horas seguidas de aula',
+      ],
+      price: '49,00',
+    },
+    {
+      image: pran2,
+      title: 'ADULTO',
+      tags: [
+        'Público entre 5 e 15 anos',
+        'equipamentos fornecidos',
+        'Horários de sexta a sábado',
+        '2 horas seguidas de aula',
+      ],
+      price: '49,00',
+    },
+    {
+      image: pran3,
+      title: 'PROFISSIONAL',
+      tags: [
+        'Público entre 5 e 15 anos',
+        'equipamentos fornecidos',
+        'Horários de sexta a sábado',
+        '2 horas seguidas de aula',
+      ],
+      price: '49,00',
+    },
+  ];
+
   return(
-    <div>
-      <h4>Aulas</h4>
+    <div className="container-cards">
+        <img src={ waves } alt="icone de ondas" className="icon-wave" />
+        <h4 className="title-cards">Aulas</h4>
+        <div className="cards">
+          {cardsData.map((card) => (
+            <div className="card">
+              <img src={card.image} alt="icone de uma prancha" className="icon-card" />
+              <h4 className="title-card">{card.title}</h4>
+              <div className="tags-card">
+                {card.tags.map((tag) => (
+                  <span>{tag}</span>
+                ))}
+              </div>
+              <div className="card-price">{`R$ ${card.price} / Aula`}</div>
+              <button className="button-registration">
+                MATRICULE-SE<img src={setButton} alt="icone de botão" className="image-button" />
+              </button>
+            </div>
+          ))}
+        </div>
     </div>
   );
 }
--- surfbot-origamid/src/pages/Main.jsx
+++ surfbot-origamid/src/pages/Main.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import Description from '../components/Description';
 import Header from '../components/Header';
 import Banner from '../components/Banner';
