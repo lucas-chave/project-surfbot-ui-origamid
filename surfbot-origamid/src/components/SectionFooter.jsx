a2391d0b48fc1e54fc5705a0ef9e02b1d0fb8c3a
lucas-chave <lcchavesr150@gmail.com>
2021-10-14 14:01:45 -0300

    adding footer

--- surfbot-origamid/src/components/SectionFooter.jsx
+++ surfbot-origamid/src/components/SectionFooter.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import icon from '../images/prancha.svg';
+import '../css/footer.css';
 
 function SectionFooter() {
   return (
-    <div>
-      d
+    <div className="container-footer">
+      <p className="elements-footer">Surfnet 2017 - alguns direitos reservados..</p>
+      <img  className="elements-footer" src={ icon } alt="icone de prancha" />
     </div>
   );
 }
