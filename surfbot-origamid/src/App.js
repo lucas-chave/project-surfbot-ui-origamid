a712c5b6a7d39981fa2b4792da2616dc57cdd49e
lucas-chave <lcchavesr150@gmail.com>
2021-10-15 13:29:17 -0300

    adding register route

--- surfbot-origamid/src/App.js
+++ surfbot-origamid/src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route } from 'react-router-dom';
 import Main from './pages/Main';
+import Register from './pages/Register';
 import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
       <Route exact path="/" component={ Main } />
+      <Route exact path="/register" component={ Register} />
     </BrowserRouter>
   );
 }
