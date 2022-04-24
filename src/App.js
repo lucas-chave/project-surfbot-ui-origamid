ed956109cc0aa805bfdc00d8096a472b5c14e42f
lucas-chave <lcchavesr150@gmail.com>
2022-04-24 02:31:30 -0300

    fix: cleaning code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { BrowserRouter, Route } from 'react-router-dom';
 import Main from './pages/Main';
 import Register from './pages/Register';
-import './App.css';
 
 function App() {
   return (
